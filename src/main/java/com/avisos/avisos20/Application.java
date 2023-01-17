7e9e3fda3633f4bd0ad4e082e27c2e1413bd8b60
caiorodriveira <rodrigocaio2775@gmail.com>
2023-01-17 13:27:21 -0300

    IMPROVEMENT - aplicação de documentação de API com swagger

--- src/main/java/com/avisos/avisos20/Application.java
+++ src/main/java/com/avisos/avisos20/Application.java
@@ -3,6 +3,11 @@ package com.avisos.avisos20;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+@OpenAPIDefinition(info = @Info(title = "Avisos", description = "API do projeto do sistema de Avisos", version = "2.0"))
 @SpringBootApplication
 public class Application {
 
