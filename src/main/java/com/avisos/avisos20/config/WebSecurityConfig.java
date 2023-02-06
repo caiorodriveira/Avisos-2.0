edf5cda81afea3a5838f28dd8a803ad1be8a4e8b
caiorodriveira <rodrigocaio2775@gmail.com>
2023-02-06 14:55:26 -0300

    REFACTOR - altera nome do pacote de configuração para config

--- src/main/java/com/avisos/avisos20/config/WebSecurityConfig.java
+++ src/main/java/com/avisos/avisos20/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.avisos.avisos20.security;
+package com.avisos.avisos20.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
