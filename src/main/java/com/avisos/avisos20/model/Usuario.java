91881c7b9dfc26f0cab4b425b24d0663f928b393
caiorodriveira <rodrigocaio2775@gmail.com>
2023-02-06 15:18:12 -0300

    REFACTOR - alteração de model de usuario para aplicação do security

--- src/main/java/com/avisos/avisos20/model/Usuario.java
+++ src/main/java/com/avisos/avisos20/model/Usuario.java
@@ -1,10 +1,17 @@
 package com.avisos.avisos20.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -19,11 +26,21 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_usuario")
 	private Long id;
-	@Column(nullable = false, length = 20)
-	private String usuario;
 	@Column(nullable = false)
 	private String nome;
+	@Column(nullable = false, unique = true, length = 20)
+	private String username;
 	@Column(nullable = false)
 	private String password;
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "tb_user_roles", joinColumns = @JoinColumn(name = "id_usuario"))
+	@Column(name = "role_id")
+	private List<String> roleStrings = new ArrayList<>();
+	
+	public Usuario() {}
+	
+    public Usuario(String username){
+        this.username = username;
+    }
 	
 }
